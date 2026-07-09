d457a29a5a7abe18e764cfa0125086d2c939b3f2
fullloopcrm <fullloopcrm@gmail.com>
2026-07-09 05:26:48 -0400

    fix(seo): drop dead tenant-seo imports resurrected by merge (broke Vercel build) [deploy]

--- platform/src/lib/seo/tenant-seo.ts
+++ platform/src/lib/seo/tenant-seo.ts
@@ -6,12 +6,6 @@
  * To onboard a tenant: add `src/lib/seo/tenants/<slug>.ts` (calls
  * registerTenantSeo with that tenant's data) and one import line below.
  */
-import './tenants/the-florida-maid'
-import './tenants/the-home-services-company'
-import './tenants/we-pay-you-junk'
-import './tenants/nyc-tow'
-import './tenants/nycroadsideemergencyassistance'
-import './tenants/theroadsidehelper'
 
 export { TENANT_SEO, buildTenantSitemap, registerTenantSeo } from './tenant-sitemap'
 export type { TenantSeoDescriptor, UrlSpec, ChangeFreq } from './tenant-sitemap'
